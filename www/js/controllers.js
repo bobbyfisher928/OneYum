802609843df3833e37713e56fa8f97175ed91344
Bobby Fisher <r.e.fisher.jr@gmail.com>
2015-10-11 15:53:00 -0400

    Minor css changes and screen view console messages for changing classes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,6 +2,15 @@ angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
+  var screen = window.innerWidth;
+  console.log(screen);
+
+  if (screen < 641) {
+    console.log('mobile');
+  } else {
+    console.log('larger');
+  };
+
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
