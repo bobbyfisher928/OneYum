5d8d82a294e928967cb419b00ca12e2850b103a3
Bobby Fisher <r.e.fisher.jr@gmail.com>
2015-11-01 18:41:43 -0500

    same variable, more modification

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,12 +2,12 @@ angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout, $state) {
 
-  console.log(window.innerWidth < 800);
+  console.log(window.innerWidth < 650);
   // console.log(screen);
   $scope.setClass = function() {
     var screen = window.innerWidth;
     console.log(screen);
-    if (screen < 800) {
+    if (screen < 650) {
       return true;
       // console.log('mobile');
     } else {
