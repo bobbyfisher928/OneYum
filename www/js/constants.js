dd9c3a9ac1b52f2c81a6d00c569dc7a33e21c5dd
Bobby Fisher <r.e.fisher.jr@gmail.com>
2015-12-17 13:41:04 -0500

    url correction

--- www/js/constants.js
+++ www/js/constants.js
@@ -1,12 +1,12 @@
 angular.module('OneYum.constants', [])
 
 .constant('API', {
-	login: 'http://oneyum.org/server/index.php/login',
-	username: 'http://oneyum.org/server/index.php/checkusername',
-	email: 'http://oneyum.org/server/index.php/checkemail',
-	register: 'http://oneyum.org/server/index.php/register',
-	validateSupKey: 'http://oneyum.org/server/index.php/validatesupkey',
-	supregister: 'http://oneyum.org/server/index.php/supregister',
+	login: 'http://oneyum.org/www/server/index.php/login',
+	username: 'http://oneyum.org/www/server/index.php/checkusername',
+	email: 'http://oneyum.org/www/server/index.php/checkemail',
+	register: 'http://oneyum.org/www/server/index.php/register',
+	validateSupKey: 'http://oneyum.org/www/server/index.php/validatesupkey',
+	supregister: 'http://oneyum.org/www/server/index.php/supregister',
 })
 
 .constant('PopupFill', {
