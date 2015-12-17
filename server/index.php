290b99833b28ea25266c312324b457f864f7c47e
Bobby Fisher <r.e.fisher.jr@gmail.com>
2015-12-17 15:20:32 -0500

    complete revert back to old code.

--- server/index.php
+++ server/index.php
@@ -2,8 +2,7 @@
 
 // Allow from any origin
 if (isset($_SERVER['HTTP_ORIGIN'])) {
-    // header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN'],"*"}");
-    header("Access-Control-Allow-Origin, "*" ");
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
     header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Max-Age: 86400');    // cache for 1 day
 }
@@ -15,7 +14,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
         header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
 }
 
 // header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN'],'*'}");
