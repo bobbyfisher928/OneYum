8a3d08112b392ad9b5cfc5fbe4d5ca72da71c075
Bobby Fisher <r.e.fisher.jr@gmail.com>
2015-12-22 17:17:11 -0500

    Cookies are functional. Updated registration cookie.

--- server/index.php
+++ server/index.php
@@ -48,7 +48,7 @@ $app->post('/register', function() use ( $app ) {
   $user['fname'] = decode5t($user['fname']);
   $user['lname'] = decode5t($user['lname']);
   $response = $user;
-  $app->setCookie('oy',$user['authorize'],'2 days','/','');
+  $app->setCookie('oy',$user['authorize'],'2 days','/','oneyum.org');
   echo json_encode( $response );
 });
 
